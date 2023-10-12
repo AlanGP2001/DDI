659e16cb7c450a24b56476add6b0c784451bb223
Alan Gallardo Peñaloza <gallardopenalozaalan@gmail.com>
2023-10-12 00:24:26 -0600

    card actualizadas

--- practica2-navegation/src/components/Card/Card.js
+++ practica2-navegation/src/components/Card/Card.js
@@ -1,45 +1,55 @@
 import React from "react";
-import { View, Text, TouchableOpacity, Image, StyleSheet } from "react-native";
+import { Text, TouchableOpacity, Image, StyleSheet } from "react-native";
+import { Avatar, Card } from "react-native-paper";
 
-export default function Card(props) {
+export default function Cards(props) {
   const { characters } = props;
   const goToPersonaje = () => {
     console.log(`Conoce más del personaje: ${characters.name}`);
   };
 
   return (
+    // <TouchableOpacity onPress={goToPersonaje}>
+    //   <View style={styles.card}>
+    //     <View style={styles.content}>
+    //       <View style={styles.infoContainer}>
+    //         <Text style={styles.name}>Nombre: {characters.name}</Text>
+    //         <Text style={styles.info}>Género: {characters.gender}</Text>
+    //         <Text style={styles.info}>Especie: {characters.species}</Text>
+    //         <Text style={styles.info}>Estado: {characters.status}</Text>
+    //       </View>
+    //       <Text style={styles.number}>
+    //         {characters.id < 10 ? `#00${characters.id}` : `#0${characters.id}`}
+    //       </Text>
+    //       <Image style={styles.imagen} source={{ uri: characters.image }} />
+    //     </View>
+    //   </View>
+    // </TouchableOpacity>
     <TouchableOpacity onPress={goToPersonaje}>
-      <View style={styles.card}>
-        <View style={styles.content}>
-          <View style={styles.infoContainer}>
-            <Text style={styles.name}>Nombre: {characters.name}</Text>
-            <Text style={styles.info}>Género: {characters.gender}</Text>
-            <Text style={styles.info}>Especie: {characters.species}</Text>
-            <Text style={styles.info}>Estado: {characters.status}</Text>
-          </View>
-          <Text style={styles.number}>
+      <Card.Title
+        style={styles.card}
+        title={characters.name}
+        subtitle={characters.species}
+        left={(props) => (
+          <Avatar.Image size={45} source={{ uri: characters.image }} />
+        )}
+        right={(props) => (
+          <Text style={styles.text}>
             {characters.id < 10 ? `#00${characters.id}` : `#0${characters.id}`}
           </Text>
-          <Image style={styles.imagen} source={{ uri: characters.image }} />
-        </View>
-      </View>
+        )}
+      />
     </TouchableOpacity>
   );
 }
 
 const styles = StyleSheet.create({
   card: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    padding: 16,
-    backgroundColor: "#82A652",
-    borderWidth: 1,
-    borderColor: "#282828",
-    borderRadius: 30,
-    marginBottom: 16,
-    marginEnd: 10,
+    marginEnd: 15,
     marginStart: 10,
+  },
+  text: {
+    color: '#79B547'
   },
   content: {
     flexDirection: "row",
